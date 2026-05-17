2299ad4ab96d69f469115a6f3515086f0a8d307e
Robin Mueller <robin.mueller.m@pm.me>
2026-05-18 00:09:51 +0200

    additive async I2C

--- src/i2c_interface.rs
+++ src/i2c_interface.rs
@@ -8,7 +8,6 @@ pub struct I2CDisplayInterface(());
 
 impl I2CDisplayInterface {
     /// Create new builder with a default I2C address of 0x3C
-    #[cfg(not(feature = "async"))]
     #[allow(clippy::new_ret_no_self)]
     // pub fn with_i2c<I>(i2c: I) -> I2CInterface<I> // alternative, but breaking change
     pub fn new<I>(i2c: I) -> I2CInterface<I>
@@ -17,17 +16,17 @@ impl I2CDisplayInterface {
     {
         Self::new_custom_address(i2c, 0x3C)
     }
+
     #[cfg(feature = "async")]
     #[allow(clippy::new_ret_no_self)]
     /// Create a new async I2C interface with the address 0x3C
-    pub fn new<I>(i2c: I) -> I2CInterface<I>
+    pub fn new_async<I>(i2c: I) -> I2CInterface<I>
     where
         I: embedded_hal_async::i2c::I2c,
     {
-        Self::new_custom_address(i2c, 0x3C)
+        Self::new_custom_address_async(i2c, 0x3C)
     }
 
-    #[cfg(not(feature = "async"))]
     /// Create a new I2C interface with the alternate address 0x3D as specified in the datasheet.
     pub fn new_alternate_address<I>(i2c: I) -> I2CInterface<I>
     where
@@ -38,14 +37,13 @@ impl I2CDisplayInterface {
 
     #[cfg(feature = "async")]
     /// Create a new async I2C interface with the alternate address 0x3D as specified in the datasheet.
-    pub fn new_alternate_address<I>(i2c: I) -> I2CInterface<I>
+    pub fn new_alternate_address_async<I>(i2c: I) -> I2CInterface<I>
     where
         I: embedded_hal_async::i2c::I2c,
     {
-        Self::new_custom_address(i2c, 0x3D)
+        Self::new_custom_address_async(i2c, 0x3D)
     }
 
-    #[cfg(not(feature = "async"))]
     /// Create a new I2C interface with a custom address.
     pub fn new_custom_address<I>(i2c: I, address: u8) -> I2CInterface<I>
     where
@@ -55,7 +53,7 @@ impl I2CDisplayInterface {
     }
     #[cfg(feature = "async")]
     /// Create a new  async I2C interface with a custom address.
-    pub fn new_custom_address<I>(i2c: I, address: u8) -> I2CInterface<I>
+    pub fn new_custom_address_async<I>(i2c: I, address: u8) -> I2CInterface<I>
     where
         I: embedded_hal_async::i2c::I2c,
     {
